f8fc872bfba7356fa93f3442c83991fd0f817c1d
Igor Bubelov <igor@bubelov.com>
2018-11-21 12:30:23 +0700

    Refactor scripts

---
+++ compare.py
@@ -0,0 +1,40 @@
+import sys
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+import alpha_vantage
+
+def compare(stock_symbol, benchmark_symbol, interval='MONTHLY'):
+    stock_data = alpha_vantage.get_stock_price_history(stock_symbol, interval, adjusted=True)
+    benchmark_data = alpha_vantage.get_stock_price_history(benchmark_symbol, interval, adjusted=True)
+
+    stock_data = { k:v for (k,v) in stock_data.items() if k in benchmark_data.keys() }
+    benchmark_data = { k:v for (k,v) in benchmark_data.items() if k in stock_data.keys() }
+
+    stock_data = adjust_values(stock_data)
+    benchmark_data = adjust_values(benchmark_data)
+
+    plt.plot(stock_data.keys(), stock_data.values(), label=stock_symbol)
+    plt.plot(benchmark_data.keys(), benchmark_data.values(), label=benchmark_symbol)
+
+    title = '%s vs %s (adjusted)' % (stock_symbol, benchmark_symbol)
+    plt.title(title)
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.legend()
+    plt.show()
+
+def adjust_values(data, start=100.0):
+    scale_factor = None
+
+    for k, v in sorted(data.items()):
+        if scale_factor == None:
+            scale_factor = v / start
+
+        data[k] = v / scale_factor
+
+    return data
+
+compare(sys.argv[1], sys.argv[2])
