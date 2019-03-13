302ff692b6c2516dceaffc192571225153eeb66f
Igor Bubelov <igor@bubelov.com>
2019-03-13 19:58:12 +0700

    Update history script

--- history.py
+++ history.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import alpha_vantage
 
 def show_history(symbol, interval='MONTHLY'):
-    data = alpha_vantage.get_stock_price_history(symbol, interval)
+    data = alpha_vantage.get_stock_price_history(symbol, interval, adjusted=False)
     plt.plot(list(data.keys()), list(data.values()))
 
     plt.title('%s price history' % symbol)
--- install-dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Installing dependencies..."
-pip install -r requirements.txt
-echo "Done!"
